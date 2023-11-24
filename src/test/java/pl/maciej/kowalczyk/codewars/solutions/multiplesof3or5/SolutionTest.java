278bde352f9ed8647c6208eeeaf259eada1b0482
Maciej Kowalczyk <mm.kowalczykk@gmail.com>
2023-11-24 14:47:41 +0100

    Added new problem Multiples of 3 or 5 https://www.codewars.com/kata/514b92a657cdc65150000006/train/java

---
+++ src/test/java/pl/maciej/kowalczyk/codewars/solutions/multiplesof3or5/SolutionTest.java
@@ -0,0 +1,13 @@
+package pl.maciej.kowalczyk.codewars.solutions.multiplesof3or5;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        assertEquals(23, new Solution().solution(10));
+    }
+
+}
