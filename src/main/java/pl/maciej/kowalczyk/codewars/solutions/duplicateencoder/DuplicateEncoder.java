94246ee6656015a343dd1198d0636935640e016f
Maciej Kowalczyk <mm.kowalczykk@gmail.com>
2023-11-25 17:43:46 +0100

    My solution to problem

--- src/main/java/pl/maciej/kowalczyk/codewars/solutions/duplicateencoder/DuplicateEncoder.java
+++ src/main/java/pl/maciej/kowalczyk/codewars/solutions/duplicateencoder/DuplicateEncoder.java
@@ -18,9 +18,37 @@ Assertion messages may be unclear about what they display in some languages.
 If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
  */
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DuplicateEncoder {
     static String encode(String word) {
+        word = word.toLowerCase();
+        Map<Character, Integer> characters = convertToMap(word);
 
-        return word;
+        String result = "";
+
+        for (int i = 0; i < word.length(); i++){
+            if (characters.get(word.charAt(i)) > 1) {
+                result += ")";
+            } else {
+                result += "(";
+            }
+        }
+        return result;
+    }
+
+    private static Map<Character, Integer> convertToMap(String word) {
+        Map<Character, Integer> characters = new HashMap<>();
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+
+            if (!characters.containsKey(c)) {
+                characters.put(c, 1);
+            } else {
+                characters.put(c, characters.get(c) + 1);
+            }
+        }
+        return characters;
     }
 }
--- src/test/java/pl/maciej/kowalczyk/codewars/solutions/duplicateencoder/DuplicateEncoderTest.java
+++ src/test/java/pl/maciej/kowalczyk/codewars/solutions/duplicateencoder/DuplicateEncoderTest.java
@@ -4,9 +4,6 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.runners.JUnit4;
-
-
 public class DuplicateEncoderTest {
     @Test
     public void test() {
