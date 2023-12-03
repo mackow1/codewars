c1616f9c2cb55b7caba9d98986092aad83eb6816
Maciej Kowalczyk <mm.kowalczykk@gmail.com>
2023-12-03 20:19:58 +0100

    Added new problem Simple Pig Latin https://www.codewars.com/kata/520b9d2ad5c005041100000f/train/java

---
+++ src/main/java/pl/maciej/kowalczyk/codewars/solutions/simplepiglatin/PigLatin.java
@@ -0,0 +1,18 @@
+package pl.maciej.kowalczyk.codewars.solutions.simplepiglatin;
+
+/*
+https://www.codewars.com/kata/520b9d2ad5c005041100000f/train/java
+
+Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
+Examples
+pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+pigIt('Hello world !');     // elloHay orldway !
+ */
+
+public class PigLatin {
+    public static String pigIt(String str) {
+        // Write code here
+        return null;
+    }
+}
