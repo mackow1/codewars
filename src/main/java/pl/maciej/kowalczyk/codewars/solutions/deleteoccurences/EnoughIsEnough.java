b7d69e4e7aec35b8eb58a5927bf8db3515c9039f
mackow1 <115844078+mackow1@users.noreply.github.com>
2023-12-26 15:39:45 +0100

    Added new problem

---
+++ src/main/java/pl/maciej/kowalczyk/codewars/solutions/deleteoccurences/EnoughIsEnough.java
@@ -0,0 +1,14 @@
+package pl.maciej.kowalczyk.codewars.solutions.deleteoccurences;
+
+/*
+
+ */
+
+public class EnoughIsEnough {
+
+	public static int[] deleteNth(int[] elements, int maxOccurrences) {
+
+		return null;
+	}
+
+}
