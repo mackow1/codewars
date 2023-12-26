622c6c0eda3e395e607e44a9c5331c10b1b6f591
mackow1 <115844078+mackow1@users.noreply.github.com>
2023-12-26 15:18:14 +0100

    New problem added

---
+++ src/main/java/pl/maciej/kowalczyk/codewars/solutions/spiningwords/SpinWords.java
@@ -0,0 +1,23 @@
+package pl.maciej.kowalczyk.codewars.solutions.spiningwords;
+
+/*
+https://www.codewars.com/kata/5264d2b162488dc400000001/train/java
+
+Write a function that takes in a string of one or more words, and returns the same string,
+but with all words that have five or more letters reversed (Just like the name of this Kata).
+Strings passed in will consist of only letters and spaces.
+Spaces will be included only when more than one word is present.
+
+Examples:
+
+"Hey fellow warriors"  --> "Hey wollef sroirraw"
+"This is a test        --> "This is a test"
+"This is another test" --> "This is rehtona test"
+ */
+
+public class SpinWords {
+
+    public String spinWords(String sentence) {
+        return null;
+    }
+}
